f2427c2a8f9441d848f8af251c34e720f005376d
Shulbhi-Gupta <shulbhi18@gmail.com>
2022-06-14 14:36:51 +0530

    first_react_website

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import BannerImage from './components/BannerImage';
+import AlbumMaker from './components/AlbumMaker';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BannerImage />
+        <div>
+          <h1>Welcome to my photojoural</h1>
+          <h3>Harrison Kong</h3>
+       <AlbumMaker />
+        </div>
       </header>
     </div>
   );
